94d8c0f3b4936a2b8b3e777d222ca27a68eaa1c4
sandersoovli <sander.soovali@voco.ee>
2023-01-19 15:45:17 +0200

    pma_paigaldus.sh

--- pma_paigaldus.sh
+++ pma_paigaldus.sh
@@ -5,7 +5,7 @@
 #muutuja sisse:
 PMA=$(dpkg-query -W -f='${Status}' phpmyadmin 2>/dev/null | grep -c 'ok instlled')
 #kui PMA muutuja väärtus võrdub 0-ga
-if [$PMA -eq 0 ]; then
+if [ $PMA -eq 0 ]; then
 	#siis ok installed ei ole leitud
 	#ja väljastame vastav teade ning
 	#paigaldame teenus
@@ -13,10 +13,11 @@ if [$PMA -eq 0 ]; then
 	apt install phpmyadmin
 	echo "phpmyadmin on paigaldatud"
 #kui PMA muutuja väärtus võrdub 1-ga
-elif [$PMA -eq 1 ]; then
+elif [ $PMA -eq 1 ]; then
 	#siis ok installed on leitud 1 kord
 	#ja teenus on juba paigaldatud
 	echo "phpmyadmin on juba paigaldatud"
+	apt list --installed 2>/dev/null | grep phpmyadmin
 	#kontrollime olemasolu
 #lõpetame tingimuslause
 fi
